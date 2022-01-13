c220d5074a5b30df5974fbc86a9b7cdfef25665f
UhDgayeong <gygy3686@naver.com>
2022-01-14 02:53:43 +0900

    CardStackView 구현

---
+++ app/src/main/java/com/example/blinddate/slider/CardStackAdapter.kt
@@ -0,0 +1,31 @@
+package com.example.blinddate.slider
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.blinddate.R
+
+class CardStackAdapter(val context : Context, val items : List<String>) : RecyclerView.Adapter<CardStackAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardStackAdapter.ViewHolder {
+        val inflater = LayoutInflater.from(parent.context) // 아까 만들어 준 아이템 뷰를 넣어줌
+        val view : View = inflater.inflate(R.layout.item_card, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: CardStackAdapter.ViewHolder, position: Int) {
+        // 데이터를 넣어주는 부분
+        holder.binding(items[position])
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    inner class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+        fun binding(data : String) {
+
+        }
+    }
+}
