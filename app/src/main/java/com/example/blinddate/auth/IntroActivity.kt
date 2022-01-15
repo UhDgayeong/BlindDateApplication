92c535ccdd775f7755c94c02493fb08fafe1a48c
UhDgayeong <gygy3686@naver.com>
2022-01-16 04:23:48 +0900

    splash화면, intro화면 구현

---
+++ app/src/main/java/com/example/blinddate/auth/IntroActivity.kt
@@ -0,0 +1,12 @@
+package com.example.blinddate.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.blinddate.R
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+    }
+}
